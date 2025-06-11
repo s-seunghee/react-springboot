f16cb398d8894b70d6a48d39622e75d3be5231ac
shs07230 <shs07230@intobe.co.kr>
2025-06-11 19:01:26 +0900

    backend file add

---
+++ src/main/java/com/example/reactspringboot/test/TestController.java
@@ -0,0 +1,28 @@
+package com.example.reactspringboot.test;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class TestController {
+    @PostMapping(value="/testData")
+    public Map<Integer, String> testData(@RequestBody List<String> params){
+        Map<Integer, String> data = new HashMap<>();
+        data.put(1,"사과");
+        data.put(2,"포도1111");
+        data.put(3,"바나나");
+
+        int i = 4;
+        for(String param : params){
+            data.put(i, param);
+            i++;
+        }
+
+        return data;
+    }
+}
